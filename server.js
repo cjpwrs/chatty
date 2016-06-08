e50298a2dc58a386a284cc60a1ba69f7aa739982
CJ Powers <cjpwrs@gmail.com>
2016-06-08 16:06:13 -0600

    finished product

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = 3001;
+
+var responseHeaders = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }
+
+var messages = [{
+    "message": "Greetings",
+    "time": null
+}
+    
+];
+
+app.use(bodyParser.json())
+app.listen(port, function(){
+    console.log('listening on port ', port);
+});
+
+app.options('/', function(req, res){
+    res.status(200).set(responseHeaders).send(JSON.stringify(messages))
+});
+
+app.get('/', function(req, res){
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+})
+
+app.post('/', function(req, res){
+    messages.push(
+        {"message": req.body.message,
+         "time": new Date()
+        });
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+})
 
